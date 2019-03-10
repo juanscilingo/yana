20f464a9c1e59e00236207c4f14e12686bd8f6b4
Juan Manuel Scilingo <juanscilingo@gmail.com>
2019-03-10 02:42:57 -0300

    Fixing axios problems

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+const { REACT_APP_API_URL } = process.env;
+
 const authApi = {
   login: credentials =>
-    axios.post("/auth/signin", credentials).then(response => response.data),
-  signout: () => axios.post("/auth/signout")
+    axios
+      .post(`${REACT_APP_API_URL}/auth/signin`, credentials)
+      .then(response => response.data),
+  signout: () => axios.post(`${REACT_APP_API_URL}/auth/signout`)
 };
 
 export default authApi;
